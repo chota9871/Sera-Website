e21fe78141bc098a450a502ae316b30ff391ec46
chota9871 <deshwalsun@gmail.com>
2025-08-17 12:56:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def contact():
     return render_template('contact.html')  # Contact page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Important: host='0.0.0.0' to accept external traffic on Render
+    app.run(host='0.0.0.0', port=5000, debug=True)
